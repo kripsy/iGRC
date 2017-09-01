fc5baee1fff195051444616339bb1bc9203e8495
egn <kripsy93@yandex.ru>
2017-09-01 20:11:40 +0300

    PY-2016.3.2 <egn@MacBook-Air-Evgenij.local Overwrite remote https://github.com/kripsy/iGRC to local

--- README.md
+++
@@ -1,3 +0,0 @@
-# egrc
-GRC concept
-if some problem with version -> https://github.com/kripsy/iGRC is a copy 
--- mainmenu/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from mainmenu.views import menu
-
-
-urlpatterns = [
-    url(r'^', menu, name = 'menu'),
-#    url(r'^logout/', logout, name='logout'),
-]
